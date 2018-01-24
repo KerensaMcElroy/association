8974f633b7acda387acb2f7086854eba4f247041
McElroy, Kerensa (A&F, Black Mountain) <Kerensa.Mcelroy@csiro.au>
2018-01-25 10:56:34 +1100

    KSW40 initiated sequence analysis

---
+++ src/sequence.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#***************************************************************#
+#                           sequence.sh                         #
+#                  written by Kerensa McElroy                   #
+#                          January 2016				#
+#                                                               #
+#               processes raw data sequence data                #
+#***************************************************************#
+
+export TODAY=$(date +%Y-%m-%d_%H-%M-%S)
+
+#-------------------environmental variables---------------------#
+
+export BIG_DATA=/OSM/CBR/AG_FUTUREFOREST/data
+export BIG_ANALYSIS=$FLUSHDIR/AG_FUTUREFOREST/
+export BIG_RESULTS=/OSM/CBR/AG_FUTUREFOREST/results
